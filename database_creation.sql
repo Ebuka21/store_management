d4f6e92be3e192f1e25c212ff2229ef6366c9291
Ebuka <ife_ebuka@ymail.com>
2022-06-15 22:44:15 +0100

    add more to files

--- database_creation.sql
+++ database_creation.sql
@@ -1,3 +1,5 @@
+/*Table creation*/
+
 CREATE TABLE category(
     categoryname VARCHAR(10),
     categoryKey INT PRIMARY KEY
@@ -13,4 +15,60 @@ CREATE TABLE product(
         FOREIGN KEY(categoryKey)
             REFERENCES category(categoryKey)
 );
+
+
+CREATE TABLE test_purchase(
+    id SERIAL,
+    customerKey INT,
+    product VARCHAR(20),
+    quantity INT
+);
+
+
+--obtain a certain number of letters from the word
+SELECT SUBSTR(productname,1,3)
+FROM product
+LIMIT 5;
+
+--adding an extra column to the table
+ALTER TABLE test_purchase
+ADD COLUMN pk INT;
+
+--converting the extra column into a foreign key
+ALTER TABLE test_purchase
+ADD FOREIGN KEY(pK)
+REFERENCES product(productkey);
+
+
+SELECT * FROM test_purchase;
+
+INSERT INTO test_purchase(customerkey,product,quantity,pk)
+VALUES
+(23,'Egg',50,1),
+(24,'Indomie',40,1);
+
+/*Procedures*/
+DROP PROCEDURE update_product;
+CREATE PROCEDURE update_product(item VARCHAR(20))
+LANGUAGE SQL
+AS $$
+UPDATE product
+SET quantity = quantity - (SELECT quantity FROM test_purchase WHERE product = item)
+WHERE productname = item;$$
+
+CALL update_product('Indomie');
+
+
+/*Views*/
+CREATE MATERIALIZED VIEW test_view
+AS
+    SELECT productname, quantity
+    FROM
+    product
+WITH DATA;
+
+REFRESH MATERIALIZED VIEW test_view;
+
+SELECT * FROM test_view;
+
 
